2c633f76430cc3cf36150bef110e2b99f57c3aa2
mrkamin <108926875+mrkamin@users.noreply.github.com>
2024-08-06 09:40:29 +0500

    Display genres

--- src/components/GameGride.tsx
+++ src/components/GameGride.tsx
@@ -11,9 +11,9 @@ const GameGride = () => {
     <>
       {error && <Text>{error}</Text>}
       <SimpleGrid
-        columns={{ sm: 1, md: 2, lg: 3 }}
+        columns={{ sm: 1, md: 2, lg: 3, xl: 5 }}
         padding={10}
-        spacing={10}
+        spacing={3}
       >
         {isLoding &&
           skeletons.map((skeleton) => (
