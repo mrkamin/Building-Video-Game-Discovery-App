833bf772c73042bf1c29e39d50e104732f60f67f
Rafi <mrkamin2@gmail.com>
2024-08-13 08:43:57 +0500

    Add a dynamic page heading

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { Grid, GridItem, HStack, Show } from "@chakra-ui/react";
+import { Box, Grid, GridItem, HStack, Show } from "@chakra-ui/react";
 import NavBar from "./components/NavBar";
 import GameGride from "./components/GameGride";
 import GenreList from "./components/GenreList";
@@ -7,6 +7,7 @@ import { Genre } from "./hooks/useGenres";
 import PlatformSelector from "./components/PlatformSelector";
 import { Platform } from "./hooks/useGames";
 import SortSelector from "./components/SortSelector";
+import GameHeading from "./components/GameHeading";
 
 export interface GameQuery {
   genre: Genre | null;
@@ -39,7 +40,9 @@ function App() {
         </GridItem>
       </Show>
       <GridItem area="main">
-        <HStack spacing={5} paddingLeft={9}>
+        <Box paddingLeft={9}>
+        <GameHeading gameQuery={gameQuery} />
+        <HStack spacing={5} >
           {" "}
           <PlatformSelector
             selectedPlatform={gameQuery.platform}
@@ -54,6 +57,7 @@ function App() {
             }
           />
         </HStack>
+        </Box>
 
         <GameGride gameQuery={gameQuery} />
       </GridItem>
